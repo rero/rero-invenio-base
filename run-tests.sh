dc440903a04d48f37d212afd089a455644168e5b
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2022-05-12 13:45:44 +0200

    cli: add utils commands
    * Adds check JSON indentation.
    * Adds check licence in files.
    
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -14,7 +14,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 # Usage:
@@ -33,11 +33,19 @@ function cleanup() {
     echo "Done"
 }
 trap cleanup EXIT
+safety check
+flask rero utils check_license check_license_config.yml
+pydocstyle rero_ils tests docs
+isort --check-only --diff rero_invenio_base tests
+autoflake -c -r --remove-all-unused-imports --ignore-init-module-imports . &> /dev/null || {
+    autoflake --remove-all-unused-imports -ri --ignore-init-module-imports .
+    exit 1
+}
 
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 # Note: for now we do not need this for the tests.
-# eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
+eval "$(docker-services-cli up --search ${SEARCH:-elasticsearch} --env)"
 python -m pytest
 tests_exit_code=$?
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
